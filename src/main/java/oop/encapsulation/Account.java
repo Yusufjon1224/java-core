51b040ec5dc52815069aeec80d98f5a8e7015519
Yusufjon Axmedov <163830632+Yusufjon1224@users.noreply.github.com>
2026-02-12 01:17:43 +0500

    wip bankaccount

--- src/main/java/oop/encapsulation/Account.java
+++ src/main/java/oop/encapsulation/Account.java
@@ -8,6 +8,20 @@ public class Account {
     private String customerEmail;
     private String customerPhone;
 
+    public void depositFounds(double depositAmount) {
+        balance += depositAmount;
+        System.out.println("Deposit of $ " + depositAmount + " made. New balance is $ " + balance);
+    }
+
+    public void withDrawFunds (double withDrawlAmount) {
+        if (balance - withDrawlAmount < 0) {
+            System.out.println("Insufficient Funds! You only have $ " + balance + " in your account");
+        } else {
+            balance -= withDrawlAmount;
+            System.out.println("Withdrawal of $ " + withDrawlAmount + " processed, Remaining balance  = $" + balance);
+        }
+    }
+
     public String getNumber() {
         return number;
     }
--- src/main/java/oop/encapsulation/ClassesChallengeBankAccount.java
+++ src/main/java/oop/encapsulation/ClassesChallengeBankAccount.java
@@ -4,5 +4,24 @@ public class ClassesChallengeBankAccount {
 
     public static void main (String[] args) {
 
+        Account digosAccount = new Account();
+
+        digosAccount.setNumber("54321");
+        digosAccount.setBalance(1000.0);
+        digosAccount.setCustomerName("Diyor Mavlonov");
+        digosAccount.setCustomerEmail("myemail@digo.com");
+        digosAccount.setNumber("(+998) 99-945-12-30");
+
+        digosAccount.withDrawFunds(100.0);
+        digosAccount.depositFounds(250.0);
+        digosAccount.withDrawFunds(50.0);
+
+        digosAccount.withDrawFunds(200.0);
+
+        digosAccount.depositFounds(100.0);
+        digosAccount.withDrawFunds(45.55);
+        digosAccount.withDrawFunds(54.46);
+
+        digosAccount.withDrawFunds(54.45);
     }
 }
