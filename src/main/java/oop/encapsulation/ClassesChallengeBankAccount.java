25514755980111da7aef3ee4137a9ca50be88bed
Yusufjon Axmedov <163830632+Yusufjon1224@users.noreply.github.com>
2026-02-12 17:54:32 +0500

    impl cons'

--- src/main/java/oop/encapsulation/ClassesChallengeBankAccount.java
+++ src/main/java/oop/encapsulation/ClassesChallengeBankAccount.java
@@ -4,13 +4,16 @@ public class ClassesChallengeBankAccount {
 
     public static void main (String[] args) {
 
-        Account digosAccount = new Account();
+        Account digosAccount = new Account("54321", 1000.0, "Diyor Mavlonov",
+                "myemail@digo.com", "(+998) 99-945-12-30");
 
-        digosAccount.setNumber("54321");
-        digosAccount.setBalance(1000.0);
-        digosAccount.setCustomerName("Diyor Mavlonov");
-        digosAccount.setCustomerEmail("myemail@digo.com");
-        digosAccount.setNumber("(+998) 99-945-12-30");
+        System.out.println(digosAccount.getNumber());
+        System.out.println(digosAccount.getBalance());
+//        digosAccount.setNumber("54321");
+//        digosAccount.setBalance(1000.0);
+//        digosAccount.setCustomerName("Diyor Mavlonov");
+//        digosAccount.setCustomerEmail("myemail@digo.com");
+//        digosAccount.setNumber("(+998) 99-945-12-30");
 
         digosAccount.withDrawFunds(100.0);
         digosAccount.depositFounds(250.0);
