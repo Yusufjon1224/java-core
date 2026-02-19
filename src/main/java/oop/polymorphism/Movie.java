b33217d608e0c0c0746f63d7c4e63365b42cf32f
Yusufjon Axmedov <163830632+Yusufjon1224@users.noreply.github.com>
2026-02-19 22:39:47 +0500

    implementation of polymorphism

---
+++ src/main/java/oop/polymorphism/Movie.java
@@ -0,0 +1,79 @@
+package oop.polymorphism;
+
+public class Movie {
+
+    private String title;
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public void watchMovie() {
+
+        String instanceType = this.getClass().getSimpleName();
+        System.out.println(title + " is a " + instanceType + " film ");
+    }
+
+    public static Movie getMovie(String type, String title) {
+        return switch (type.toUpperCase().charAt(0)) {
+            case 'A' -> new Adventure(title);
+            case 'C' -> new Comedy(title);
+            case 'S' -> new ScienceFiction(title);
+            default -> new Movie(title);
+        };
+    }
+}
+
+
+class Adventure extends Movie {
+
+    // this is constructor
+    public Adventure(String title) {
+        super(title);
+    }
+
+
+    // this is overriding the methods
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n".repeat(3),
+                "Pleasant Scene",
+                "Scary Music",
+                "Something Bad Happens");
+    }
+}
+
+class Comedy extends Movie {
+
+    // this is constructor
+    public Comedy(String title) {
+        super(title);
+    }
+
+    // this is overriding the methods
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n".repeat(3),
+                "Something funny happens",
+                "Something even funnier happens",
+                "Happy ending");
+    }
+}
+
+class ScienceFiction extends Movie {
+
+    public ScienceFiction(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n".repeat(3),
+                "Bad Aliens Do Bad Stuff",
+                "Space Guy Chase Aliens",
+                "Planet Blows Up");
+    }
+}
